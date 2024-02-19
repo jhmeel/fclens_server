501f6aff8e568add8fd038664063c133c66e00db
Jhmeel <muhammadjhmeel01@gmail.com>
2024-02-19 08:37:24 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -10,22 +10,28 @@ import RoutesProvider from "./providers/routesProvider.js";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+
 class Core {
   constructor() {
     this.isRunning = false;
     this.app = express();
     this.PORT = Config.PORT;
+    this.initMiddlewares(); // Move middleware initialization to the constructor
   }
 
   initMiddlewares() {
     try {
+      this.app.use(cors({
+        origin: "*",
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+        optionSuccessStatus: 200,
+      }));
       this.app.use(helmet());
       this.app.disable("x-powered-by");
       this.app.use(bodyParser.json());
       this.app.use(bodyParser.urlencoded({ extended: true }));
-      this.app.use(
-        cors({ origin: "*", credentials: true, optionSuccessStatus: 200 })
-      );
       this.app.use((req, res, next) => {
         logger.info(req.headers);
         next();
@@ -42,7 +48,6 @@ class Core {
   }
 
   bootstrap() {
-    this.initMiddlewares();
     this.initRoutes();
     DB.init().then(() => {
       this.app.listen(this.PORT, () => {
